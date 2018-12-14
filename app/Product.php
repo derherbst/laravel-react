9aaa3759fb1aee95b2e576e19bf7146014ac5253
Georgy Gerbst <g.gerbst@it.ecommpay.com>
2018-12-14 16:43:02 +0300

    added react and Main component

--- app/Product.php
+++ app/Product.php
@@ -1,42 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App;
 
-use Illuminate\Http\Request;
-use App\Product;
+use Illuminate\Database\Eloquent\Model;
 
-class ProductsController extends Controller
+class Product extends Model
 {
+    //
 
-    public function index()
-    {
-        return Product::all();
-    }
-
-    public function show(Product $product)
-    {
-        return $product;
-    }
-
-    public function store(Request $request)
-    {
-        $product = Product::create($request->all());
-
-        return response()->json($product, 201);
-    }
-
-    public function update(Request $request, Product $product)
-    {
-        $product->update($request->all());
-
-        return response()->json($product, 200);
-    }
-
-    public function delete(Product $product)
-    {
-        $product->delete();
-
-        return response()->json(null, 204);
-    }
-
+    protected $fillable = ['title', 'description', 'price', 'availability'];
 }
